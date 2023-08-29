256938b30ed50db6bdd116df96879ec54168752a
abhijith.mr <abhijithgowda1998@gmail.com>
2023-08-29 20:41:14 +0530

    commit

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -6,6 +6,7 @@ import Container from '@/components/ui/container';
 import useCart from '@/hooks/use-cart';
 
 import CartItem from './components/cart-item';
+import Summary from "./components/summary";
 
 export const revalidate = 0;
 
